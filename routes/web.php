8346a213d24da087f79ea943bc73ddb9e7ca5560
hodotomo <sapoaferie@gmail.com>
2023-09-04 22:50:29 +0900

    a

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;      //追加
+Route::get('/', [PostController::class, 'index']);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/', function(){
+    return view('posts.index');
 });
