d7c9fe1c6c0e32ad99bea2fb12a121306258c31a
hodotomo <sapoaferie@gmail.com>
2023-09-11 00:20:37 +0900

    8-5

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,7 +8,7 @@
     </head>
     <body>
         <h1 class="title">
-            {{$post -> title}}
+            {{$post->title}}
         </h1>
         <div class="content">
             <div class="content_post">
@@ -16,6 +16,7 @@
                 <p>{{$post->body}}</p>
             </div>
         </div>
+        <div class="edit"><a href="/posts/{{$post->id}}/edit">edit</a></div>
         <div class="footer">
             <a href="/">戻る</a>
         </div>
